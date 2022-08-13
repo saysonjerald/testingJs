702b6cbd6aa8de7b057e9d5cdd6974fceb5c12cc
Jerald Sayson <saysonjerald@gmail.com>
2022-08-13 22:36:33 +0800

    Added: NEW! toEqual();

--- index.test.js
+++ index.test.js
@@ -2,6 +2,7 @@ import {it, expect, describe} from 'vitest';
 import {add} from './add';
 
 
+//#region  describe(), it(), expect(), tobe..();
 describe("Addition testing", () => {
 
     it("should summarize number values of an array", () => {
@@ -29,4 +30,21 @@ it("Checking if it is a number", () => {
     const input = 123;
 
     expect(input).toBeTypeOf('number');
-}) 
+})
+//#endregion
+
+describe('Reference and Premitive values', () => {
+    it('Testing a reference value', () => {
+        //reference: Arrays, Object & Function
+        const value = [1, 2, 3, 99];
+
+        //When comparing, it pointes to the pointer location rather the value
+        console.log(value === [1, 2, 3, 99]); // <== false, even though the same value
+        
+        //👉 Code: expect(value).toBe([1, 2, 3, 99]);
+        //👉 Result: AssertionError: expected [ 1, 2, 3, 99 ] to be [ 1, 2, 3, 99 ] // Object.is equality 
+
+        //Instead of using to .toBe(), We're going to use .toEqual() for reference values;
+        expect(value).toEqual([1, 2, 3, 99]);
+    });
+})
